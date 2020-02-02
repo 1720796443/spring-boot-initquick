0879a44f69171c268947f4438a675213247e0063
lcs <1720796443@qq.com>
2020-02-02 17:39:40 +0800

    刚刚这是添加错了

---
+++ src/test/java/com/li/springbootinitquick/SpringBootInitquickApplicationTests.java
@@ -0,0 +1,17 @@
+package com.li.springbootinitquick;
+
+import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitquickApplicationTests {
+     Logger logger = LoggerFactory.getLogger(getClass());
+	@Test
+	void contextLoads() {
+
+	}
+
+}
